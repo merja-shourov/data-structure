889d0ced7eec4b9c9999b8226bd644765eb731ca
merja-shourov <merjashourov13@gmail.com>
2025-02-22 11:47:54 +0600

    new

---
+++ banary-tree/binaryTreeTraverser.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    Node *left;
+    Node *right;
+    int val;
+
+    Node(int val ){
+        this->val = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// root - left - right
+void preorder(){
+
+}
+// left - root - right
+void inorder(){
+
+}
+
+// left-right-root
+void postorder(){
+
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    Node *root = new Node(10); 
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(70);
+    Node *f = new Node(80);
+    Node *g = new Node(60);
+    Node *h = new Node(90);
+
+    // connection
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    d->left = g;
+    b->left = e;
+    b->right = f;
+    e->left = h;
+
+    // traversal
+    preorder();
+    inorder();
+    postorder();
+
+    return 0;
+}
